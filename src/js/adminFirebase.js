a7cfd36791abc331db4723ac731b653b3c016dbc
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2024-08-03 23:17:02 +0400

    Book Adding To Firebase

--- src/js/adminFirebase.js
+++ src/js/adminFirebase.js
@@ -1,4 +1,4 @@
-import { readDataFromDB , readSingleDataFromDB,deleteDataFromDB, updateUserData ,pushDataToDB } from "./firebase.js";
+import { readDataFromDB, readSingleDataFromDB, deleteDataFromDB, updateUserData, pushDataToDB } from "./firebase.js";
 
 const addBookBtn = document.querySelector(".addBookBtn");
 const addStoreBtn = document.getElementById("addStoreBtn");
@@ -15,18 +15,18 @@ loginBtn.addEventListener("click", () => {
 });
 
 function LoginCheck() {
-    let username= (usrnameInput.value).trim();
-    let password = (passInput.value).trim();
+  let username = (usrnameInput.value).trim();
+  let password = (passInput.value).trim();
 
-    readSingleDataFromDB('users/').then((data) => {
-     
-      if (username == data.username && password == data.password) {
-        openPanel();
-      }else{
-        alert("Wrong username or password");
-      }
-     })
-    
+  readSingleDataFromDB('users/').then((data) => {
+
+    if (username == data.username && password == data.password) {
+      openPanel();
+    } else {
+      alert("Wrong username or password");
+    }
+  })
+
 }
 
 function openPanel() {
@@ -37,6 +37,31 @@ function openPanel() {
 
 
 addBookBtn.addEventListener("click", () => {
- 
-  
+  let bookTitle = document.querySelector(".bookTitle").value;
+  let bookAuthor = document.querySelector(".bookAuthor").value;
+  let bookDesc = document.querySelector(".bookDesc").value;
+  let bookImg = document.querySelector(".bookUrl").value;
+  let isNewCheck = document.querySelector("#isNewCheck").checked;
+  let isBestCheck = document.querySelector("#isBestCheck").checked;
+
+  let genreArr = []
+  let genreSelect = document.querySelectorAll(".genreSelect");
+
+  for (let i = 0; i < genreSelect.length; i++) {
+    genreArr.push(genreSelect[i].value);
+  }
+
+  console.log(bookTitle, bookAuthor, bookDesc, genreArr, bookImg);
+  let newBook = {
+    title: bookTitle,
+    author: bookAuthor,
+    description: bookDesc,
+    img: bookImg,
+    isNew: isNewCheck,
+    isBestSeller: isBestCheck,
+    genre: genreArr
+  }
+
+  pushDataToDB('books/', newBook);
+  alert("Book added successfully");
 });
