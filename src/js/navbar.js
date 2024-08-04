2a453a573141b2a6975ec8283c955d6e224f81c8
Sedi98 <sedi9816@gmail.com>
2024-08-05 00:29:31 +0400

    added responsive navigation

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,9 @@
 const joinUs = document.querySelector(".joinUs");
 const joinUsContainer = document.querySelector(".joinUsContainer");
 const joinCloseBtn  = document.querySelector(".joinCloseBtn");
+const hamburger = document.querySelector(".hamburger");
+const navUl = document.querySelector(".navUl");
+const navClose = document.querySelector(".navClose");
 
 
 joinUs.addEventListener("click", () => {
@@ -19,4 +22,17 @@ joinUsContainer.addEventListener("click", (e) => {
 
 joinCloseBtn.addEventListener("click", () => {
     joinUsContainer.style = "display: none";
+})
+
+
+hamburger.addEventListener('click',()=>{
+    navUl.style = "top: 0";
+    
+    
+})
+
+navClose.addEventListener('click',()=>{
+    navUl.style = "top: -100vh";
+    
+    
 })
