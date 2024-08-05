723964fec5ace7bd4944f292baa61ee98ebfd01f
nerminbalayeva <nerminbalayeva1@icloud.com>
2024-08-05 13:00:19 +0300

    contact form

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,22 +1,28 @@
+import { pushDataToDB } from "./firebase.js";
+
+
 const nameInput = document.querySelector('#nameinput')
 const emailinput = document.querySelector('#emailinput')
 const adressInput = document.querySelector('#adressInput')
 const phoneinput = document.querySelector('#phoneinput')
 const noteinput = document.querySelector('#noteinput')
 
+const textInput = document.querySelectorAll('.textinput')
 
 const textCount = document.querySelector('.textCount')
 const textlimit = document.querySelector('.textlimit')
 const buttton = document.querySelector('.buttton') 
                     
+const wordLimit= 100
 
+textlimit.textContent = wordLimit
 
 
 noteinput.addEventListener('input', () => {
 
 
     if (noteinput.value.length > 100) {
-        noteinput.value = noteinput.value.slice(0, 100)
+        noteinput.value = noteinput.value.slice(0, wordLimit)
         textCount.textContent = noteinput.value.length
         textCount.style="color:red"
     }else{
@@ -26,6 +32,49 @@ noteinput.addEventListener('input', () => {
     
     textCount.textContent = noteinput.value.length
    
+})
+
+
+
+buttton.addEventListener('click', () => { 
+
+    let errCount= 0;
+
+    for (let i = 0; i < textInput.length; i++) {
+        if (textInput[i].value === "") {
+            textInput[i].style = "border: 1px solid red;"
+            errCount++;
+        }
+    }
+
+    if (noteinput.value=="") {
+        noteinput.style = "border: 1px solid red;"
+        
+    }
+
+
+    if (errCount > 0) {
+        alert("Please fill in all fields")
+        return
+
+    }
+
+// push data if all inputs are filled
+    pushDataToDB("contact/", {
+        name: (nameInput.value).trim(),
+        email: (emailinput.value).trim(),
+        adress: (adressInput.value).trim(),
+        phone: (phoneinput.value).trim(),
+        note: (noteinput.value).trim(),
+    })
+    alert("Thank you for your message. We will get back to you as soon as possible.")
+
+
+
+    
+    for (let i = 0; i < textInput.length; i++) {
+        textInput[i].value = ""
+    }
 })
 
 
