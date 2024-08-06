60f8ce0208b32ed6d231fd133cbc1a67c8e79b4b
Sedi98 <sedi9816@gmail.com>
2024-08-06 20:03:34 +0400

    admin page is fully fixed and ready for use

--- src/js/adminFirebase.js
+++ src/js/adminFirebase.js
@@ -33,7 +33,6 @@ const addNewGenreBtn = document.querySelector(".addNewGenreBtn");
 
 let bookGenres = [];
 
-
 // about store section variables
 const aboutBooktitle = document.querySelector(".aboutBookTitle");
 const aboutBookImg = document.querySelector(".aboutBookImg");
@@ -46,9 +45,9 @@ function loadGenres() {
   readDataFromDB("genres/").then((data) => {
     data.forEach((genre) => {
       bookGenres.push(genre.name);
-      genreSelect.innerHTML += `<option value="${genre.name}">${genre.name}</option>`
-    })
-  })
+      genreSelect.innerHTML += `<option value="${genre.name}">${genre.name}</option>`;
+    });
+  });
 }
 
 loadGenres();
@@ -83,12 +82,29 @@ addBookBtn.addEventListener("click", () => {
   let bookImg = document.querySelector(".bookUrl").value;
   let isNewCheck = document.querySelector("#isNewCheck").checked;
   let isBestCheck = document.querySelector("#isBestCheck").checked;
+  //
+  let addBookInput = document.querySelectorAll(".addBookInput");
+  let isEmpty = false;
+
+  for (let j = 0; j < addBookInput.length; j++) {
+    addBookInput[j].style = "border: 1px solid #ccc";
+    
+  }
+
+  for (let i = 0; i < addBookInput.length; i++) {
+    if (addBookInput[i].value === "") {
+      addBookInput[i].style = "border: 1px solid red;";
+      isEmpty = true;
+    }
+  }
+
+  if (isEmpty) {
+    alert("Please fill in all fields");
+    return;
+  }
 
   let genreArr = [];
   let genreSelect = document.querySelectorAll(".genreSelect");
-
-
-
 
   for (let i = 0; i < genreSelect.length; i++) {
     genreArr.push(genreSelect[i].value);
@@ -108,10 +124,14 @@ addBookBtn.addEventListener("click", () => {
   alert("Book added successfully");
 });
 
+
+// new genre modal cancel button
 modalClose.addEventListener("click", () => {
   modalContainer.style = "display: none";
 });
 
+
+// new genre modal add button
 modalAddBtn.addEventListener("click", () => {
   if (modalInput.value !== "") {
     pushDataToDB("genres/", { name: modalInput.value });
@@ -123,10 +143,8 @@ modalAddBtn.addEventListener("click", () => {
 });
 
 
-
-
+// add a new genre selection in admin page
 addGenreBtn.addEventListener("click", () => {
-
   let select = document.createElement("select");
   select.classList.add("genreSelect");
   select.classList.add("addBookInput");
@@ -141,25 +159,19 @@ addGenreBtn.addEventListener("click", () => {
   genreContainer.appendChild(select);
 
 
-
-
+  // if multiple genre select generated then show remove genre button
   let genreSelect = document.querySelectorAll(".genreSelect");
-  console.log(genreSelect.length);
-
   if (genreSelect.length > 1) {
     removeGenreBtn.style.display = "flex";
   } else {
     removeGenreBtn.style.display = "none";
   }
+});
 
-})
-
-
+// remove multiple genre select
 removeGenreBtn.addEventListener("click", () => {
   genreContainer.removeChild(genreContainer.lastElementChild);
 
-
-
   let genreSelect = document.querySelectorAll(".genreSelect");
   console.log(genreSelect.length);
   if (genreSelect.length > 1) {
@@ -167,57 +179,53 @@ removeGenreBtn.addEventListener("click", () => {
   } else {
     removeGenreBtn.style.display = "none";
   }
-})
-
-addNewGenreBtn.addEventListener("click", () => {
-
-  modalContainer.style = "display: flex";
-
 });
 
 
+// show new genre modal when click
+addNewGenreBtn.addEventListener("click", () => {
+  modalContainer.style = "display: flex";
+});
 
-// about store section functions
-
+// function for change about section
 addStoreBtn.addEventListener("click", () => {
   let errorCount = 0;
   for (let i = 0; i < addStoreInput.length; i++) {
     if (addStoreInput[i].value === "") {
-      addStoreInput[i].style = "border: 1px solid red;"
+      addStoreInput[i].style = "border: 1px solid red;";
       errorCount++;
-
     }
-
   }
 
-
   if (errorCount > 0) {
-    alert("Please fill in all fields")
-    return
-
+    alert("Please fill in all fields");
+    return;
   } else {
-
-
     writeSingleDataToDB("aboutStore/", {
       title: aboutBooktitle.value,
       description: aboutBookDescription.value,
-      img: aboutBookImg.value
-    })
-    alert("About store changed successfully")
+      img: aboutBookImg.value,
+    });
+    alert("About store changed successfully");
   }
-
-
 });
 
+
+
+
+// show books list on book list table
 function bookTableAdd() {
   let bookTableBody = document.querySelector(".bookTableBody");
-  readDataFromDB('books/').then((data) => {
+  readDataFromDB("books/").then((data) => {
     console.log(data);
-    bookTableBody.innerHTML = data.map((item) => {
-      return `
+    bookTableBody.innerHTML = data
+      .map((item) => {
+        return `
       <tr>
-                <td>${data.indexOf(item)+1}</td>
-                <td class="bookListTitle"><img class="addBooksImg" src="${item.img}">${item.title}</td>
+                <td>${data.indexOf(item) + 1}</td>
+                <td class="bookListTitle"><img class="addBooksImg" src="${
+                  item.img
+                }">${item.title}</td>
                 <td > <p class="minDesc"> ${item.description}</p> </td>
                 <td>${item.genre[0]}</td>
                 <td>${item.author}</td>
@@ -225,10 +233,62 @@ function bookTableAdd() {
               </tr>
 
       
-      `
-    }).join("")
-
-  })
+      `;
+      })
+      .join("");
+  });
 }
 
-bookTableAdd()
+
+function renderJoinUsData() {
+
+  let joinUsTableBody = document.querySelector(".joinUsTableBody");
+ 
+  readDataFromDB("join/").then((data) => {
+    
+    
+    joinUsTableBody.innerHTML = data
+      .map((item) => {
+        return `
+      <tr>
+          <td>${data.indexOf(item) + 1}</td>
+          <td>${item.name}</td>
+          <td>${item.email}</td>
+          <td><i class="bx bx-trash trashIcon"></i></td>
+      </tr>
+      `;
+      })
+      .join("");
+  });
+}
+
+
+function renderContactUsData() {
+
+  let contactTableBody = document.querySelector(".contactTableBody");
+ 
+  readDataFromDB("contact/").then((data) => {
+  console.log(data);
+  
+    
+    contactTableBody.innerHTML = data
+      .map((item) => {
+        return `
+      <tr>
+          <td>${data.indexOf(item) + 1}</td>
+          <td>${item.name}</td>
+          <td>${item.adress}</td>
+          <td>${item.email}</td>
+          <td>${item.phone}</td>
+          <td> <p class="minDesc">${item.note} </p> </td>
+          <td><i class="bx bx-trash trashIcon"></i></td>
+      </tr>
+      `;
+      })
+      .join("");
+  });
+}
+
+bookTableAdd();
+renderJoinUsData();
+renderContactUsData();
