9bf413d6f93d6b44049959f26d123140bd53fce7
nerminbalayeva <nerminbalayeva1@icloud.com>
2024-08-06 22:23:03 +0300

    catalog age is fully active

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -3,8 +3,10 @@ import { readDataFromDB } from "./firebase.js";
 const bookSlideLine = document.querySelectorAll(".bookSlideLine");
 const sliderNextImg = document.querySelectorAll(".sliderNextImg");
 const sliderPreviusImg = document.querySelectorAll(".sliderPreviusImg");
-const card = document.querySelectorAll(".card");
+let card = document.querySelectorAll(".card");
 const categoryList = document.querySelector(".categoryList");
+
+let bookCategories;
 
 console.log(card[0].offsetWidth);
 
@@ -23,7 +25,6 @@ for (let i = 0; i < bookSlideLine.length; i++) {
 let newBooksList = [];
 let bestBookList = [];
 
-
 function buttonHider() {
   console.log(bookSlideLine[0].scrollWidth);
   console.log(bookSlideLine[1].scrollWidth);
@@ -40,29 +41,130 @@ function buttonHider() {
       bookSlideLine[i].scrollWidth
     ) {
       sliderNextImg[i].style = "display: none";
-    } else if(bookSlideLine[i].scrollWidth == bookSlideLine[i].offsetWidth) {
+    } else if (bookSlideLine[i].scrollWidth == bookSlideLine[i].offsetWidth) {
       sliderNextImg[i].style = "display: none";
-
     } else {
       sliderNextImg[i].style = "display: block";
     }
-   
   }
 }
 
-function getBooks() {
-  readDataFromDB("books").then((data) => {
-    data.forEach((item) => {
-      if (item.isNew == true) {
-        newBooksList.push(item);
-      }
-      if (item.isBestSeller == true) {
-        bestBookList.push(item);
-      }
-    });
-    console.log(newBooksList);
-    console.log(bestBookList);
+async function loadCategories() {
+  categoryList.innerHTML = `<a data-name="All" class="bookCategories">All</a>`;
 
+  let data = await readDataFromDB("genres/");
+
+  console.log(data);
+
+  categoryList.innerHTML += data
+    .map((item) => {
+      return `<a data-name="${item.name}" class="bookCategories">${item.name}</a>`;
+    })
+    .join("");
+
+  loadBooks("All", data);
+
+  bookCategories = document.querySelectorAll(".bookCategories");
+
+  for (let i = 0; i < bookCategories.length; i++) {
+    bookCategories[i].addEventListener("click", () => {
+      loadBooks(bookCategories[i].dataset.name);
+    });
+  }
+}
+
+loadCategories();
+getAllBooks();
+
+async function getAllBooks() {
+  let data = await readDataFromDB("books/");
+
+  data?.forEach((item) => {
+    if (item.isNew == true) {
+      newBooksList.push(item);
+    }
+    if (item.isBestSeller == true) {
+      bestBookList.push(item);
+    }
+  });
+
+  renderPriorityBooks(bestBookList, newBooksList);
+  loadBooks("All", data);
+}
+
+async function loadBooks(param = "All", bookData) {
+  let data = bookData || (await readDataFromDB("books/"));
+  
+  if (param != "All") {
+    data = data.filter((item) => item.genre.includes(param));
+    console.log(data);
+  }
+
+  if (data.length != 0) {
+    renderAllBooks(data);
+  } else {
+    renderAllBooks([]);
+  }
+}
+
+function renderPriorityBooks(bestBookData, newBooksData) {
+  bookSlideLine[1].innerHTML = bestBookData
+    .map((item) => {
+      return `
+
+        <div div class="card" >
+            ${item.isNew == true ? '<div class="newIcon"><p>NEW</p></div>' : ""}
+
+            <img
+              src=${item.img}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${item.author}</p>
+              <a href="../pages/bookInfo.html" onclick="setBookInfo(${
+                item.id
+              })" class="btn btn-primary"
+                >Read More</a
+              >
+            </div>
+          </div >
+                `;
+    })
+    .join("");
+
+  bookSlideLine[2].innerHTML = newBooksData
+    .map((item) => {
+      return `
+
+        <div div class="card" >
+            ${item.isNew == true ? '<div class="newIcon"><p>NEW</p></div>' : ""}
+
+            <img
+              src=${item.img}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${item.author}</p>
+              <a href="../pages/bookInfo.html" onclick="setBookInfo(${
+                item.id
+              })" class="btn btn-primary"
+                >Read More</a
+              >
+            </div>
+          </div >
+                `;
+    })
+    .join("");
+}
+
+function renderAllBooks(data) {
+  if (data.length == 0) {
+    bookSlideLine[0].innerHTML = "<p style='text-align: center'>No Books Found</p>";
+  } else {
     bookSlideLine[0].innerHTML = data
       .map((item) => {
         return `
@@ -88,64 +190,9 @@ function getBooks() {
                 `;
       })
       .join("");
+  }
 
-    bookSlideLine[1].innerHTML = bestBookList
-      .map((item) => {
-        return `
-
-        <div div class="card" >
-            ${item.isNew == true ? '<div class="newIcon"><p>NEW</p></div>' : ""}
-
-            <img
-              src=${item.img}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">${item.title}</h5>
-              <p class="card-text">${item.author}</p>
-              <a href="../pages/bookInfo.html" onclick="setBookInfo(${
-                item.id
-              })" class="btn btn-primary"
-                >Read More</a
-              >
-            </div>
-          </div >
-                `;
-      })
-      .join("");
-
-    bookSlideLine[2].innerHTML = newBooksList
-      .map((item) => {
-        return `
-
-        <div div class="card" >
-            ${item.isNew == true ? '<div class="newIcon"><p>NEW</p></div>' : ""}
-
-            <img
-              src=${item.img}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">${item.title}</h5>
-              <p class="card-text">${item.author}</p>
-              <a href="../pages/bookInfo.html" onclick="setBookInfo(${
-                item.id
-              })" class="btn btn-primary"
-                >Read More</a
-              >
-            </div>
-          </div >
-                `;
-      })
-      .join("");
-
-
-      buttonHider();
-  });
-
- 
+  buttonHider();
 }
 
-getBooks();
+// getBooks();
