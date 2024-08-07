1e2c0918f32c527730d9a6b88c7bc76ff49e744b
Sedi98 <sedi9816@gmail.com>
2024-08-07 19:18:17 +0400

    book info page book details are loading from database for now. added moment js

--- src/js/bookInfo.js
+++ src/js/bookInfo.js
@@ -1,22 +1,20 @@
 import { pushDataToDB, readSingleDataFromDB } from "./firebase.js";
 
-
-
 const aboutBookSection = document.querySelector(".aboutBookSection");
+console.log(new Date().toISOString());
 
+let SpecId = localStorage.getItem("bookId");
 
-let SpecId=localStorage.getItem("bookId")
-
-SpecId= SpecId.replace(/"/g, '')
+SpecId = SpecId.replace(/"/g, "");
 bookInfo();
 console.log(SpecId);
 
 async function bookInfo() {
-  let data= await readSingleDataFromDB(`books/${SpecId}`)
+  let data = await readSingleDataFromDB(`books/${SpecId}`);
 
   console.log(data);
 
-  aboutBookSection.innerHTML =`
+  aboutBookSection.innerHTML = `
   
   <button class="backBtn"><a href="../pages/catalog.html"> < Back</a></button>
       <div class="aboutBook">
@@ -24,20 +22,20 @@ async function bookInfo() {
           <div class="yearBox">${new Date().getFullYear()}</div>
           <div class="bookInformation">
             <h1>${data.title}</h1>
-            <h2>2 days ago added</h2>
-            <p class="author">Konstantin Koptelov</p>
+            <h2>${moment(`${data.addDate}`, "YYYYMMDD").fromNow()}</h2>
+            <p class="author">${data.author}</p>
             <p class="description">
               ${data.description}. More details
             </p>
           </div>
         </div>
         <div class="aboutBookRightside">
-          <div class="newSign">New</div>
+        ${data.isNew == true ? '<div class="newSign">New</div>' : ""}
+          
           <img class="aboutBookImage" src="${data.img}" alt="" />
         </div>
       </div>
   
   
-  `
-  
+  `;
 }
